1e7b2526929292e9e725e902c095fe16611ff3a6
Wiktor Ziejewski <wiktor.ziejewski>
2022-11-13 16:50:25 +0100

    clear code

--- node_modules/fizzy-ui-utils/test/unit/filter-find-elements.js
+++
@@ -1,8 +0,0 @@
-QUnit.test( 'filterFindElements', function( assert ) {
-
-  let gridB = document.querySelector('.grid-b');
-
-  let itemElems = fizzyUIUtils.filterFindElements( gridB.children, '.item' );
-  assert.equal( itemElems.length, 4, '4 items filter/found' );
-
-} );
--- node_modules/flickity/test/unit/watch.js
+++
@@ -1,9 +0,0 @@
-QUnit.test( 'watch fallback', function( assert ) {
-
-  let elem = document.querySelector('#watch');
-  let flkty = new Flickity( elem, {
-    watchCSS: true,
-  } );
-
-  assert.ok( !flkty.isActive, 'fallback not active, watchCSS: true' );
-} );
